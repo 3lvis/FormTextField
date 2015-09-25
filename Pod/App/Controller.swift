c1b1a55ef257b775b36399cfb6cbfcc3c77a547c
Elvis Nuñez <elvisnunez@me.com>
2015-09-25 22:18:36 +0200

    TextField is enabled by default

--- Pod/App/Controller.swift
+++ Pod/App/Controller.swift
@@ -15,7 +15,6 @@ class Controller: UIViewController {
         let emailTextField = TextField(frame: CGRect(x: margin, y: 60, width: self.view.frame.width - (margin * 2.0), height: 45))
         emailTextField.inputTypeString = "email"
         emailTextField.placeholder = "Email"
-        emailTextField.enabled = true
         self.view.addSubview(emailTextField)
 
         var previousFrame = emailTextField.frame
@@ -23,7 +22,6 @@ class Controller: UIViewController {
         let phoneNumberTextField = TextField(frame: previousFrame)
         phoneNumberTextField.inputTypeString = "phone"
         phoneNumberTextField.placeholder = "Phone number"
-        phoneNumberTextField.enabled = true
         phoneNumberTextField.formatter = PhoneNumberFormatter()
         self.view.addSubview(phoneNumberTextField)
     }
