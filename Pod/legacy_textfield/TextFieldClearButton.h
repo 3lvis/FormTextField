91ba85b715e80a922bbb0a391c16a8d88c436220
Elvis Nuñez <elvisnunez@me.com>
2015-09-29 15:15:59 +0200

    Add legacy

---
+++ Pod/legacy_textfield/TextFieldClearButton.h
@@ -0,0 +1,15 @@
+@import UIKit;
+
+typedef NS_ENUM(NSInteger, TextFieldButtonType) {
+    TextFieldButtonTypeClear = 0,
+    TextFieldButtonTypePlus,
+    TextFieldButtonTypeMinus
+};
+
+@interface TextFieldClearButton : UIView
+
+- (instancetype)initWithFrame:(CGRect)frame andButtonType:(TextFieldButtonType)type color:(UIColor *)color;
+
++ (UIImage *)imageForSize:(CGSize)size andButtonType:(TextFieldButtonType)type color:(UIColor *)color;
+
+@end
