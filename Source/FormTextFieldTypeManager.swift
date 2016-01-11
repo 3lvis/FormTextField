3fca74635fb5feee6d0a5e8a0ffa2d9ef3c745fd
Elvis Nuñez <3lvis@users.noreply.github.com>
2016-01-11 16:42:36 +0100

    secureTextEntry false by default

--- Source/FormTextFieldTypeManager.swift
+++ Source/FormTextFieldTypeManager.swift
@@ -7,56 +7,48 @@ extension UITextField {
             self.autocapitalizationType = .Sentences
             self.autocorrectionType = .Default
             self.keyboardType = .Default
-            self.secureTextEntry = false
             break
 
         case .Name:
             self.autocapitalizationType = .Words
             self.autocorrectionType = .No
             self.keyboardType = .NamePhonePad
-            self.secureTextEntry = false
             break
 
         case .Username:
             self.autocapitalizationType = .None
             self.autocorrectionType = .No
             self.keyboardType = .NamePhonePad
-            self.secureTextEntry = false
             break
 
         case .PhoneNumber:
             self.autocapitalizationType = .None
             self.autocorrectionType = .No
             self.keyboardType = .PhonePad
-            self.secureTextEntry = false
             break
 
         case .Integer:
             self.autocapitalizationType = .None
             self.autocorrectionType = .No
             self.keyboardType = .PhonePad
-            self.secureTextEntry = false
             break
 
         case .Decimal:
             self.autocapitalizationType = .None
             self.autocorrectionType = .No
             self.keyboardType = .NumberPad
-            self.secureTextEntry = false
             break
 
         case .Address:
             self.autocapitalizationType = .Words
             self.autocorrectionType = .Default
             self.keyboardType = .ASCIICapable
-            self.secureTextEntry = false
             break
 
         case .Email:
             self.autocapitalizationType = .None
             self.autocorrectionType = .No
             self.keyboardType = .EmailAddress
-            self.secureTextEntry = false
             break
 
         case .Password:
