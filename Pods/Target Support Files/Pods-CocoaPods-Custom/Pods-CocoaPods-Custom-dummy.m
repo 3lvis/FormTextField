9455917e678946ff822e19e903cf818383ade74d
Elvis Nuñez <elvisnunez@me.com>
2017-09-19 23:15:12 +0200

    Add pods to git folder

---
+++ Pods/Target Support Files/Pods-CocoaPods-Custom/Pods-CocoaPods-Custom-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CocoaPods_Custom : NSObject
+@end
+@implementation PodsDummy_Pods_CocoaPods_Custom
+@end
---
+++ Pods/Target Support Files/Pods-CocoaPods-Native/Pods-CocoaPods-Native-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CocoaPods_Native : NSObject
+@end
+@implementation PodsDummy_Pods_CocoaPods_Native
+@end
