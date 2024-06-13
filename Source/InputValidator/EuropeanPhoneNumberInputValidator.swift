e4fe54941d76a28980e48d3a9443574f4f7252c9
Elvis <elvisnunez@me.com>
2024-06-13 23:33:03 +0200

    Initial passing tests

--- Source/InputValidator/EuropeanPhoneNumberInputValidator.swift
+++ Source/InputValidator/EuropeanPhoneNumberInputValidator.swift
@@ -3,11 +3,11 @@ import Foundation
 public struct EuropeanPhoneNumberInputValidator: InputValidatable {
     public var validation: Validation?
 
-    public init(validation _: Validation? = nil) {
+    public init(validation: Validation? = nil) {
         var predefinedValidation = Validation()
         predefinedValidation.minimumLength = 8
         predefinedValidation.maximumLength = 15
-        validation = predefinedValidation
+        self.validation = predefinedValidation
     }
 
     public func validateReplacementString(_ replacementString: String?, fullString: String?, inRange range: NSRange?) -> Bool {
@@ -33,25 +33,22 @@ public struct EuropeanPhoneNumberInputValidator: InputValidatable {
         }
 
         if phoneNumber.hasPrefix("+") {
-            return true // Partial European phone number starting with "+"
+            let withoutPlus = String(phoneNumber.dropFirst())
+            // Ensure that after the +, the first digit is between 1 and 9
+            if let firstDigit = withoutPlus.first {
+                if ("1"..."9").contains(firstDigit) {
+                    return true
+                }
+            }
         }
 
         return false
     }
 
     private func validateEuropeanPhoneNumber(_ phoneNumber: String) -> Bool {
-        let phoneNumberPattern = "^\\+[1-9]\\d{7,14}$"
+        let phoneNumberPattern = "^\\+([1-9]\\d{0,2})\\d{7,14}$"
         let regex = try! NSRegularExpression(pattern: phoneNumberPattern)
         let range = NSRange(location: 0, length: phoneNumber.utf16.count)
         return regex.firstMatch(in: phoneNumber, options: [], range: range) != nil
     }
-
-    private func composedString(_ replacementString: String?, fullString: String?, inRange range: NSRange?) -> String {
-        guard let replacementString = replacementString, let fullString = fullString, let range = range else {
-            return replacementString ?? fullString ?? ""
-        }
-        let nsString = fullString as NSString
-        return nsString.replacingCharacters(in: range, with: replacementString)
-    }
 }
-
