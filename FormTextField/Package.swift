75429d6807334b200f9caf4a66a77fa3a5707274
Elvis <elvisnunez@me.com>
2024-06-14 04:25:44 +0200

    Add empty framework

---
+++ FormTextField/Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "FormTextField",
+    platforms: [
+        .iOS(.v16)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "FormTextField",
+            targets: ["FormTextField"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "FormTextField"),
+        .testTarget(
+            name: "FormTextFieldTests",
+            dependencies: ["FormTextField"]),
+    ]
+)
