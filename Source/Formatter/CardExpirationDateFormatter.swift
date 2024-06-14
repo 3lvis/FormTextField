43f6e7fd37f674fda3d87cb5b2de03cb1cd0751e
Elvis <elvisnunez@me.com>
2024-06-14 02:27:25 +0200

    Refactor

--- Source/Formatter/CardExpirationDateFormatter.swift
+++ Source/Formatter/CardExpirationDateFormatter.swift
@@ -7,41 +7,36 @@ public struct CardExpirationDateFormatter: Formattable {
             return insertSlash(at: 2, in: newString)
         } else if reverse {
             switch string.count {
-            case 0: return string
-            case 1: return string
-            case 2: return string
-            case 3: 
+            case 0, 1, 2:
+                return string
+            case 3:
                 var newString = string.replacingOccurrences(of: "/", with: "")
                 if newString.count == 3 {
                     newString = insertSlash(at: 2, in: newString)
                 }
                 return newString
-            default: return ""
+            default:
+                return ""
             }
         } else {
             switch string.count {
-            case 0: return string
-            case 1: return string
-            case 2: return "\(string)/"
+            case 0, 1:
+                return string
+            case 2:
+                return "\(string)/"
             case 3:
                 let newString = string.replacingOccurrences(of: "/", with: "")
                 return insertSlash(at: 2, in: newString)
-            default: return ""
+            default:
+                return ""
             }
         }
     }
 
-    func insertSlash(at index: Int, in string: String) -> String {
-        // Ensure the index is within the bounds of the string
+    private func insertSlash(at index: Int, in string: String) -> String {
         guard index < string.count else { return string }
-
-        // Convert the string to an array of characters for easier manipulation
         var characters = Array(string)
-
-        // Insert the "/" at the specified index
         characters.insert("/", at: index)
-
-        // Convert the character array back to a string and return it
         return String(characters)
     }
 }
