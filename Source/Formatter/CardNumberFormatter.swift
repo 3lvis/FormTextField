cf34cb0300beb74d8a3734a32eec95c2134b786c
Elvis <elvisnunez@me.com>
2024-06-14 03:24:06 +0200

    Cleaned version

--- Source/Formatter/CardNumberFormatter.swift
+++ Source/Formatter/CardNumberFormatter.swift
@@ -2,31 +2,22 @@ public struct CardNumberFormatter: Formattable {
     public init() {}
 
     public func formatString(_ string: String, reverse: Bool = false) -> String {
-        var formattedString = String()
         let normalizedString = string.replacingOccurrences(of: " ", with: "")
+        return normalizedString.addSpaceEveryFourCharacters()
+    }
+}
 
-        if reverse {
-            // If reverse is true, just return the normalized string with spaces removed
-            var idx = 0
-            for character in normalizedString {
-                if idx != 0 && idx % 4 == 0 {
-                    formattedString.append(" ")
-                }
-                formattedString.append(character)
-                idx += 1
+private extension String {
+    func addSpaceEveryFourCharacters() -> String {
+        var formattedString = ""
+        var idx = 0
+        for character in self {
+            if idx != 0 && idx % 4 == 0 {
+                formattedString.append(" ")
             }
-        } else {
-            // If reverse is false, add spaces every four characters
-            var idx = 0
-            for character in normalizedString {
-                if idx != 0 && idx % 4 == 0 {
-                    formattedString.append(" ")
-                }
-                formattedString.append(character)
-                idx += 1
-            }
+            formattedString.append(character)
+            idx += 1
         }
-
         return formattedString
     }
 }
