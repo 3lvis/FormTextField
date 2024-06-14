94a715a59918f65617be459b62ab902983daa256
Elvis <elvisnunez@me.com>
2024-06-14 05:40:55 +0200

    Correct

--- Package.swift
+++ Package.swift
@@ -18,9 +18,11 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "FormTextField"),
+            name: "FormTextField",
+            path: "FormTextField/Sources"),
         .testTarget(
             name: "FormTextFieldTests",
-            dependencies: ["FormTextField"]),
+            dependencies: ["FormTextField"],
+            path: "FormTextField/Tests"), // Specify the path to your test files if they are in a separate folder
     ]
 )
