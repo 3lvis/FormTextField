5e3a88c996968ac8fba245c5412ddfbdd6cbc4c5
Elvis <elvisnunez@me.com>
2024-06-14 03:19:03 +0200

    Add tests improve implementation

--- Source/Formatter/CardNumberFormatter.swift
+++ Source/Formatter/CardNumberFormatter.swift
@@ -4,19 +4,24 @@ public struct CardNumberFormatter: Formattable {
     public func formatString(_ string: String, reverse: Bool = false) -> String {
         var formattedString = String()
         let normalizedString = string.replacingOccurrences(of: " ", with: "")
-        if reverse {
-            formattedString = normalizedString
-        } else {
-            var idx = 0
-            var character: Character
-            while idx < normalizedString.count {
-                let index = normalizedString.index(normalizedString.startIndex, offsetBy: idx)
-                character = normalizedString[index]
 
+        if reverse {
+            // If reverse is true, just return the normalized string with spaces removed
+            var idx = 0
+            for character in normalizedString {
                 if idx != 0 && idx % 4 == 0 {
                     formattedString.append(" ")
                 }
-
+                formattedString.append(character)
+                idx += 1
+            }
+        } else {
+            // If reverse is false, add spaces every four characters
+            var idx = 0
+            for character in normalizedString {
+                if idx != 0 && idx % 4 == 0 {
+                    formattedString.append(" ")
+                }
                 formattedString.append(character)
                 idx += 1
             }
